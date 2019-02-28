4b018bd236801f5670d50cac8d2fb0f3175d651f
heistermann <maik.heistermann@uni-potsdam.de>
2019-02-28 14:34:49 +0100

    FIX: Fix broken test.

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,7 +2,7 @@ import cosmicsense as cs
 
 import numpy as np
 
-def test_n_to_theta():
-    out = cs.core.n_to_theta(1000,100.,14, 1., 1., 1500., 1000.)
-    shouldbe = 0.8333333333333334
-    assert out==shouldbe
+def test_n_to_theta_desilets():
+    out = cs.core.n_to_theta_desilets(1500., 1000.)
+    shouldbe = 0.012796463706461009
+    assert round(out, 5)==round(shouldbe, 5)
