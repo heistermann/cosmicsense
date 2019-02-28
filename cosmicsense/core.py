ce2211e62a446b23de62f3613064a635a5925c18
heistermann <maik.heistermann@uni-potsdam.de>
2019-02-28 14:09:41 +0100

    FIX: USe cph instead of total counts per 6 h

--- cosmicsense/core.py
+++ cosmicsense/core.py
@@ -15,15 +15,17 @@ to soil moisture.
    :nosignatures:
    :toctree: generated/
 
-    n_to_theta
+    n_to_theta_desilets
 
 """
 
 import numpy as np
 import pandas as pd
 
-def n_to_theta(n, n0, a0, a1, a2, rhob, rhow):
+def n_to_theta_desilets(n, n0, a0=0.0808, a1=0.372, a2=0.115, rhob=1500., rhow=1000.):
     """Convert neutron counts to theta.
+
+    Based on Desilets et al. (2010).
 
     Parameters
     ----------
@@ -40,7 +42,7 @@ def n_to_theta(n, n0, a0, a1, a2, rhob, rhow):
     rhob : float
         Soil bulk density (kg/m3)
     rhow : float
-        Density of water (/)kg/m3)
+        Density of water (kg/m3)
 
     Returns
     -------
@@ -48,7 +50,7 @@ def n_to_theta(n, n0, a0, a1, a2, rhob, rhow):
         Volumetric soil water content (m3/m3)
 
     """
-    return (a0 / (n/n0 -a1) - a2) * rhob / rhow
+    return ((a0 / (n/n0 -a1)) - a2) * rhob / rhow
 
 
 if __name__ == '__main__':
